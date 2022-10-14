7abb2783329e388c8aca1cd95623a93756235126
Ajand Mardalizad <aseedgeek@gmail.com>
2022-10-14 12:31:36 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,55 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+const darkTheme = createTheme({
+  typography: {
+    fontFamily: "Inter",
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ECB365",
+    },
+    secondary: {
+      main: "#65ecb4",
+    },
+    background: {
+      default: "#041C32",
+      paper: "#04293A",
+    },
+    error: {
+      main: "#FF5E5B",
+    },
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          borderRadius: 10,
+          boxShadow: "rgb(20 21 33 / 18%) 0px 2px 10px 0px",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: { borderRadius: 8 },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: { borderRadius: "8px !important" },
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
